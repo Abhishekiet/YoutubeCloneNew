068e0fdc519498689eeecaa130076783058f4d31
Abhishekiet <62215477+Abhishekiet@users.noreply.github.com>
2022-09-29 13:29:39 +0530

    Update SideBar.js

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,15 +1,23 @@
 import { SidebarData } from "./SidebarData"
-import { Stack,Typography } from '@mui/material'
+import { Stack,Typography,Button } from '@mui/material'
 
 
 const SideBar=({ selected,handleToggleside })=>{
     
-    const show = SidebarData.map( a=> <Stack variant='outlined' direction='row' gap={ {md:2} }
+/*    const show = SidebarData.map( a=> <Stack variant='outlined' direction='row' gap={ {md:2} }
     onClick={ ()=> handleToggleside( a.name ) }
     sx={ selected === a.name ? { color:'red' }:{ color:'white' } } key={a.name} > 
     <span style={ {marginTop:'4PX' } } > {a.icon} </span>
     <Typography variant='h6'> {a.name} </Typography> 
     </Stack> )
+*/
+
+    const show = SidebarData.map( a=> <Button variant='outlined'
+    onClick={ ()=> handleToggleside( a.name ) }
+    sx={ selected === a.name ? { color:'red', borderColor:'black' }:{ color:'white',borderColor:'black' } } key={a.name} 
+    startIcon={a.icon}>
+        <Typography sx={{ fontSize:{ md:'21px' },  }} > {a.name} </Typography>
+    </Button> )
 
     return(
         <>
