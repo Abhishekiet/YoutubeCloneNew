953c9f5baa03f33af807a6f983b64d4d20d41289
Abhishekiet <62215477+Abhishekiet@users.noreply.github.com>
2022-09-29 13:29:02 +0530

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material'
+import { Stack, Typography,Button, CardMedia } from '@mui/material'
 import logo from './youtube.png'
 
 const Card= ( {pass , handleToggle}  ) => {
@@ -6,7 +6,39 @@ const Card= ( {pass , handleToggle}  ) => {
     return(
         <>
         
-            <Stack sx={{ backgroundColor:'#101010', padding:'5px' }} m={2}
+            <Stack sx={{ backgroundColor:'#101010', padding:'5px' }} m={2} component={Button}
+              onClick={handleToggle}>
+                <CardMedia component='img' src={pass.thumbnails.high.url} alt={logo} height='300px' sx={{ width:{md:'300px',xs:'450px'} }} />
+                <Typography color='white' align='center' mt={1} variant='h5'> {pass.title.slice(0,15)} </Typography>
+            </Stack>
+        
+        </>
+    )
+}
+
+export default Card
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Stack, Typography,Button } from '@mui/material'
+import logo from './youtube.png'
+
+const Card= ( {pass , handleToggle}  ) => {
+    
+    return(
+        <>
+        
+            <Stack sx={{ backgroundColor:'#101010', padding:'5px' }} m={2} component={Button}
               onClick={handleToggle}>
                 <img src={pass.thumbnails.high.url} alt={logo} height='300px' width='300px' />
                 <Typography color='white' align='center' mt={1} variant='h5'> {pass.title.slice(0,15)} </Typography>
@@ -17,4 +49,4 @@ const Card= ( {pass , handleToggle}  ) => {
 }
 
 export default Card
-
+*/
